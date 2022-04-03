c657b545097b8ae67865d632ecd07ff56609c7df
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-04 00:02:19 +0200

    add the hand to the clock. sec, min, hour

--- lib/clock.dart
+++ lib/clock.dart
@@ -12,20 +12,61 @@ class _ClockState extends State<Clock> {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      alignment: const Alignment(0,0),
       child: Stack(
         children: [
-          Image.network(
-            "https://www.pinclipart.com/picdir/big/43-431402_graphic-stock-huge-freebie-download-for-clock-no.png",
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
+          //Clock Image without hands
+          Center(
+            child: Image.network(
+              "https://www.pinclipart.com/picdir/big/43-431402_graphic-stock-huge-freebie-download-for-clock-no.png",
+            ),
           ),
+          // Seconds
           Container(
             child: Container(
-              height: 15,
-              width: 15,
+              height: 140,
+              width: 2,
               decoration: BoxDecoration(
-                color: Colors.black,
-                borderRadius: BorderRadius.circular(50)
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.circular(10)
+              ),
+            ),
+            alignment: const Alignment(0,-0.15),
+          ),
+          //Minutes
+          Container(
+            child: Container(
+              height: 95,
+              width: 5,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(10)
+              ),
+            ),
+            alignment: const Alignment(0,-0.15),
+          ),
+          // Hours
+          Container(
+            child: Container(
+              height: 80,
+              width: 7    ,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(10)
+              ),
+            ),
+            alignment: const Alignment(0,-0.1),
+          ),
+          // Dot
+          Container(
+            child: Container(
+              height: 20,
+              width: 20,
+              decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.circular(50)
               ),
             ),
             alignment: const Alignment(0,0),
